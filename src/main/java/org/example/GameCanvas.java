7c7f8b8e62b4582854c52e98ba211200d87766d1
Wojciech <wenter2005pl@gmail.com>
2025-06-15 16:48:55 +0200

    this is a project

---
+++ src/main/java/org/example/GameCanvas.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class GameCanvas extends Canvas {
+    private GraphicsContext graphicsC;
+    public void draw(){
+
+
+    }
+}
